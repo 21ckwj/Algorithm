d45ffab66d088a7aab9599f76e63aff48151f8fa
21ckwj <21ckwj@naver.com>
2023-02-09 23:53:16 +0900

    bj step

---
+++ 백준_단계별/문자열/1157번_단어공부.py
@@ -0,0 +1,14 @@
+string = input().upper()
+
+d = dict()
+for s in string:
+    if s not in d.keys():
+        d[s] = 1
+    else:
+        d[s] +=1
+print(d)
+
+if len([k for k,v in d.items() if max(d.values())==v])==1:
+    print(max(d, key=d.get))
+else:
+    print("?")
